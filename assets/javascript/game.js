a1f719456e6fb60064f393b160099212a7dbad29
JD <jdfili14@gmail.com>
2018-11-04 21:34:29 -0800

    test

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,21 @@
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
-var options = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var lettersGuessed = [];
+
 
 
 document.onkeyup = function () {
     var userGuess = String.fromCharCode(event.keyCode).toLocaleLowerCase();
     var computerGuess = options[Math.floor(Math.random() * options.length)];
-    
+
     if (userGuess == computerGuess) {
         wins++;
         guessesLeft = 9;
         lettersGuessed = [];
     }
-   
+
     if (userGuess != computerGuess) {
         guessesLeft--;
         lettersGuessed.push(userGuess);
@@ -27,15 +28,15 @@ document.onkeyup = function () {
     }
 
 
-    var html = "<h1> The Psychic Game" +
-    "<p>Guess what letter I'm thinking of</p>" +
-    "<p>Wins: " + wins + "</p>" +
-    "<p>Losses: " + losses + "</p>" +
-    "<p>Guesses left: " + guessesLeft + "</p>" +
-    "<p>Letters guessed: " + lettersGuessed.join(", ") + "</p>";
+    var html = "<div class='header'> <h1> The Psychic Game </h1> </div>" +
+        "<div class='container'> <p>Guess what letter I'm thinking of</p>" +
+        "<p>Wins: " + wins + "</p>" +
+        "<p>Losses: " + losses + "</p>" +
+        "<p>Guesses left: " + guessesLeft + "</p>" +
+        "<p>Letters guessed: " + lettersGuessed.join(", ") + "</p> </div>";
 
     document.querySelector("#game").innerHTML = html;
-  
+
 
 
 }
