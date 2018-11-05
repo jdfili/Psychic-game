a1ffddf37468f4157464b9d655f69b3a9b322f3a
JD <jdfili14@gmail.com>
2018-11-04 19:05:07 -0800

    basically done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,18 @@
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
-var options = ["a","b"]
-var lettersGuessed = []
+var options = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var lettersGuessed = [];
 
 
 document.onkeyup = function () {
     var userGuess = String.fromCharCode(event.keyCode).toLocaleLowerCase();
     var computerGuess = options[Math.floor(Math.random() * options.length)];
-    console.log(computerGuess);
+    
     if (userGuess == computerGuess) {
         wins++;
-        alert("wins: " + wins)
+        guessesLeft = 9;
+        lettersGuessed = [];
     }
    
     if (userGuess != computerGuess) {
@@ -19,10 +20,17 @@ document.onkeyup = function () {
         lettersGuessed.push(userGuess);
     }
 
+    if (guessesLeft === 0) {
+        guessesLeft = 9;
+        losses++;
+        lettersGuessed = []
+    }
+
 
     var html = "<h1> The Psychic Game" +
     "<p>Guess what letter I'm thinking of</p>" +
     "<p>Wins: " + wins + "</p>" +
+    "<p>Losses: " + losses + "</p>" +
     "<p>Guesses left: " + guessesLeft + "</p>" +
     "<p>Letters guessed: " + lettersGuessed.join(", ") + "</p>";
 
