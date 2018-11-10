53ee7b0e6f7836e2cec977854d45ebd352110c26
JD <jdfili14@gmail.com>
2018-11-09 20:40:39 -0800

    computerGuess works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,17 +3,18 @@ var losses = 0;
 var guessesLeft = 9;
 var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var lettersGuessed = [];
-
-
+var computerGuess = options[Math.floor(Math.random() * options.length)];
 
 document.onkeyup = function () {
     var userGuess = String.fromCharCode(event.keyCode).toLocaleLowerCase();
-    var computerGuess = options[Math.floor(Math.random() * options.length)];
+    // var computerGuess = options[Math.floor(Math.random() * options.length)];
 
     if (userGuess == computerGuess) {
         wins++;
         guessesLeft = 9;
         lettersGuessed = [];
+        computerGuess = options[Math.floor(Math.random() * options.length)];
+        
     }
 
     if (userGuess != computerGuess) {
@@ -25,6 +26,8 @@ document.onkeyup = function () {
         guessesLeft = 9;
         losses++;
         lettersGuessed = []
+        computerGuess = options[Math.floor(Math.random() * options.length)];
+        
     }
 
 
@@ -36,9 +39,4 @@ document.onkeyup = function () {
         "<p>Letters guessed: " + lettersGuessed.join(", ") + "</p> </div>";
 
     document.querySelector("#game").innerHTML = html;
-
-
-
 }
-
-
